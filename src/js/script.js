032d6a33f20573c437fbd85e7e9b3558610234ee
Maks <Makssenuk9@gmail.com>
2021-01-27 09:55:34 +0200

    add feed

--- src/js/script.js
+++ src/js/script.js
@@ -1,33 +1,36 @@
-// $(document).ready(function () {
-//     $('.carousel_inner').slick({
-//         speed: 1200,
-//         adaptiveHeight: true,
-//         arrows: true,
-//         prevArrow: '<button type="button" class="slick-prev"><img src="img/second/chevron-left-solid.png"></button>',
-//         nextArrow: '<button type="button" class="slick-next"><img src="img/second/chevron-right-solid.png"></button>',
-//         responsive: [{
-//             breakpoint: 992,
-//             settings: {
-//                 dots: true,
-//                 arrows: false
-//             }
-//         }]
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/second/chevron-left-solid.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/second/chevron-right-solid.png"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+            }
+        ]
+    });
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
 
-//     });
-    
-// });
-const slider = tns({
-    container: '.carousel_inner',
-    items: 1,
-    slideBy: 'page',
-    autoplay: false,
-    controls:false,
-    nav: false
-  });
-document.querySelector('.prev').addEventListener( 'click' , function(){ 
-  slider.goTo('prev');
-});
-document.querySelector('.next').addEventListener( 'click' , function(){ 
-  slider.goTo('next');
-});
+    function toggleSlide(item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+            })
+        });
+    };
 
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
+
+});
