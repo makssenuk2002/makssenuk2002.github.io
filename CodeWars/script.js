3b1d3f31f71c3cc9181c897f028c8d94525a02c9
Maks Senuk <72966200+makssenuk2002@users.noreply.github.com>
2021-06-02 07:38:56 +0300

    Update script.js

--- CodeWars/script.js
+++ CodeWars/script.js
@@ -1,73 +1,15 @@
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 
-// function tribonacci(q,n){
-//   let array = q;
-//   if(n==0){
-//     return array;
-//   }else{
-//   for(let i = 0 ; i<n; i++){
-//       array.push(array[i]+array[i+1]+array[i+2])
-//   }
-//   console.log(array);
-//   }
-// }
-// tribonacci([12,11,31],10);
+// Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
 
 
-// function validatePIN (pin) {
-//   var num = pin;
-//   var digits = num.toString().split('');
-//   var realDigits = digits.map(Number)
-  
-//   if(realDigits.length == 4 && Number.isInteger(+pin) && pin>=0 && realDigits[realDigits.length-2].toString() != "NaN" && realDigits[realDigits.length-1].toString() != "NaN" ){
-//     console.log(true);
-//   }else if(realDigits.length == 6 && Number.isInteger(+pin) && pin>=0 && realDigits[realDigits.length-2].toString() != "NaN" && realDigits[realDigits.length-1].toString() != "NaN"){
-//     console.log(true);
-//   }else{
-//     console.log(false);  
-//   }
- 
-//   console.log(realDigits);
-// }
-// validatePIN('111 ')
-
-
-// function validatePIN (pin) {
-//   //return true or false
-//   var n = pin.length;
-//   if( n != 4 && n != 6)
-//       console.log(false);;
-//   for (var i in pin)
-//       if (pin[i] > '9' || pin[i] < '0')
-//           console.log(false);;
-//   console.log(true);
-// }
-// validatePIN('1111.0')
-
-// function disemvowel(str){
-//   var num = str;
-//   var digits = num.toString().split('');
-//   var realDigits = digits.map(String)
-
-
-//   for(let i = 0 ; i<  realDigits.length ; i++){
-//     if(realDigits[i] == "a" ){
-//        realDigits.splice(i,1)
-//     }
-//   }
-
-//   console.log(realDigits);
-// }
-// disemvowel("aaaaaaaaasdaassssaaadeeead")
-
-// function removeVowels( input ){
-//   return input.replace(/[aeiou]/gi, ""); 
-// }
-// console.log(removeVowels(`iAnHrPXuWKXj mvOopMnfyaY wjEeozwXVoZzvPFj") should equal "nHrPXWKXj mvpMnfyY wjzwXVZzvPFj`));
-
-var lines = 7;
-var str = " ";
-var star = "*";
-for(var i = 0; i < 5; i++){
-    str += star;
-    console.log(str);
-}
+function solution(number){
+    let sum = 0;
+    for(let i = 0 ; i<number ;i++){
+      if(i%3==0 ||i%5==0  ){
+        sum+= i;
+      }
+    }
+    return sum
+  }
+console.log(solution(10));
